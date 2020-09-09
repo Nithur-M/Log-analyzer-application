54595c137618ab9eb57fc5bdb72dc5a56701fdcd
Tharun <49119576+Tharunvarshanth@users.noreply.github.com>
2020-09-09 13:58:18 +0530

    Add files via upload

---
+++ Loganalyzerapp/src/main/java/kln/se/ass2/Main.java
@@ -0,0 +1,18 @@
+package kln.se.ass2;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter File Path");
+       // String userinput =  input.nextLine();
+          String userinput ="" +
+                  "logfile.log";
+
+
+    }
+}
